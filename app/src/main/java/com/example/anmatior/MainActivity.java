23cea25d521a3b46d8ce837887be7b40fa586bd0
yechao <yechao@diditaxi.com.cn>
2016-07-03 22:03:13 +0800

    create animation demo

--- app/src/main/java/com/example/anmatior/MainActivity.java
+++ app/src/main/java/com/example/anmatior/MainActivity.java
@@ -4,16 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-
-
-
+    private ImageView imageView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        button = (Button) findViewById(R.id.button);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "image clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //1. animation
+                TranslateAnimation animation = new TranslateAnimation(0,500,0,0);
+                animation.setDuration(1000);
+                animation.setFillAfter(true);
+                imageView.startAnimation(animation);
+            }
+        });
+
     }
 
 
